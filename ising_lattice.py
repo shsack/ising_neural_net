a7c697837cf2be15ac1666296e83c0c180bb9445
ssack <ssack@student.ethz.ch>
2018-11-18 17:54:52 +0100

    put parallel for loop

--- ising_lattice.py
+++ ising_lattice.py
@@ -1,31 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+from numba import njit, prange
 
 # Gives random spin configuration
 def randomstate(N):
     return 2 * np.random.randint(2, size=(N, N)) - 1
 
 # Performs one Metropolis step
+@njit(parallel=True)
 def metropolis_step(lattice, T):
-    for _ in range(N * N):
+    for _ in prange(N * N):
         x, y = np.random.randint(0, N), np.random.randint(0, N)
-        spin = lattice[x, y]
-        deltaE = 2 * spin * (lattice[(x + 1) % N, y] + lattice[x, (y + 1) % N] +
+        deltaE = 2 * lattice[x, y] * (lattice[(x + 1) % N, y] + lattice[x, (y + 1) % N] +
                        lattice[(x - 1) % N, y] + lattice[x, (y - 1) % N])
         if deltaE < 0:
-            spin *= -1
+            lattice[x, y] *= -1
         elif np.random.rand() < np.exp(-deltaE / T):
-            spin *= -1
-        lattice[x, y] = spin
-    return lattice
+            lattice[x, y] *= -1
 
 # Saves data
 def save(object, filename):
     with open(filename + '.pickle', 'wb') as f:
         pickle.dump(object, f)
 
-#--------------------------------------MAIN---------------------------------------------
 
 # Setting up simulation parameters
 N = 16
@@ -35,7 +33,7 @@ EQsteps = 2000
 ntemp = 100
 
 T = np.linspace(1., 3.5, ntemp)
-Magnetization = np.zeros(ntemp)
+magnetization = []
 
 # Initializing spins and labels
 spins, labels = np.zeros((0, N * N)), np.zeros((0, 2))
@@ -47,14 +45,14 @@ for index, temp in enumerate(T):
     lattice = randomstate(N)
 
     # Equilibrate spin lattice
-    for eq in range(EQsteps):
+    for _ in range(EQsteps):
         metropolis_step(lattice, temp)
 
     # Loop over spin configurations to collect data
     for mc in range(MCsteps):
         if mc % 200 == 0:
-            metropolis_step(lattice, temp)
             tmp.append(np.sum(lattice))
+        metropolis_step(lattice, temp)
     spins = np.vstack((spins, lattice.ravel()))
 
     # Append correct label corresponding to current spin configuration
@@ -63,19 +61,18 @@ for index, temp in enumerate(T):
     else:
         labels = np.vstack((labels, high))
 
-    Magnetization[index] = np.mean(tmp) / (N * N)
-    print(index, "out of", len(T))
+    magnetization.append(np.mean(tmp) / (N * N))
+    print('{} out of {} temperature steps'.format(index, len(T)))
 
 # Save data
 save(0.5 * (spins + 1), 'train_spins'), save(labels, 'train_labels'), save(T, 'temperature')
-#save(0.5 * (spins + 1), 'train_spins_two'), save(labels, 'train_labels_two'), save(T, 'temperature')
 print("saved data!")
 
 # Plot Monte Carlo simulation of magnetization
-plt.plot(T, abs(Magnetization), 'o', color="green")
-plt.xlabel("Temperature (T)", fontsize=20)
-plt.ylabel("Magnetization ", fontsize=20)
+plt.plot(T, abs(np.array(magnetization)), 'o', color="green")
+plt.xlabel("Temperature", fontsize=15)
+plt.ylabel("Magnetization ", fontsize=15)
 plt.grid()
-plt.show()
+plt.savefig('magnetization_MC.pdf')
 
 
